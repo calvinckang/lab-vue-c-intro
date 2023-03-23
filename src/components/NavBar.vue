f63e214b42910c11bce46b9ac1fd25a8d4b66b6c
Calvin Kang <calvinckang@gmail.com>
2023-03-23 21:19:53 +0100

    Initial commit

---
+++ src/components/NavBar.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="navbar">
+        <img class="logo" src="../assets/Ironhack_logo.png" alt="Ironhack Logo">
+        <img class="menu-icon" src="../assets/Burger_menu.png" alt="Menu">
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+    div.navbar {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+    }
+    img.logo {
+        width: 38px;
+    }
+    img.menu-icon {
+        width: 20px;
+    }
+</style>
