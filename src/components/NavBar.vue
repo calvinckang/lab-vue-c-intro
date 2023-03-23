1289bafe016caeb6f3c9f314a60681d3a35a5b32
Calvin Kang <calvinckang@gmail.com>
2023-03-23 22:21:11 +0100

    Create NavBar component

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,24 +1,27 @@
 <template>
-    <div class="navbar">
-        <img class="logo" src="../assets/Ironhack_logo.png" alt="Ironhack Logo">
-        <img class="menu-icon" src="../assets/Burger_menu.png" alt="Menu">
-    </div>
+  <nav>
+    <img
+      class="logo"
+      src="../assets/images/Ironhack_logo.png"
+      alt="Ironhack Logo"
+    />
+    <img class="menu-icon" src="../assets/images/Burger_menu.png" alt="Menu" />
+  </nav>
 </template>
 
-<script>
-
-</script>
+<script></script>
 
 <style scoped>
-    div.navbar {
-        display: flex;
-        flex-direction: row;
-        justify-content: space-between;
-    }
-    img.logo {
-        width: 38px;
-    }
-    img.menu-icon {
-        width: 20px;
-    }
+nav {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 20px 30px;
+}
+img.logo {
+  height: 38px;
+}
+img.menu-icon {
+  height: 20px;
+}
 </style>
